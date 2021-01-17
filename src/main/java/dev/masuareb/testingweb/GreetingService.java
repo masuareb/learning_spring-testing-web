222ac0cd7ac8e870a2853d491ce2438a0d52ec07
Arturo Suárez <masuareb@hotmail.com>
2021-01-17 12:42:41 -0600

    Added Greeting Service and Controller

---
+++ src/main/java/dev/masuareb/testingweb/GreetingService.java
@@ -0,0 +1,10 @@
+package dev.masuareb.testingweb;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String greet() {
+        return "Hello, World";
+    }
+}
