59bd606b09862b51152f4e693d257abc813578ee
Arturo Suárez <masuareb@hotmail.com>
2021-01-16 23:55:14 -0600

    Testing Web Guide - Initial Commit

---
+++ src/test/java/dev/masuareb/testingweb/TestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.masuareb.testingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
